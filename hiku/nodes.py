4a6c01479b4b9c7ba4823f192359db8fa01b301e
Vladimir Magamedov <vladimir@magamedov.com>
2016-01-31 00:28:34 +0200

    Added NodeVisitor and NodeTransformer classes

--- hiku/nodes.py
+++ hiku/nodes.py
@@ -62,3 +62,63 @@ class Dict(Node):
 
     def accept(self, visitor):
         return visitor.visit_dict(self)
+
+
+class NodeVisitor(object):
+
+    def visit(self, node):
+        if hasattr(node, 'accept'):
+            node.accept(self)
+        else:
+            self.generic_visit(node)
+
+    def generic_visit(self, node):
+        pass
+
+    def visit_symbol(self, node):
+        pass
+
+    def visit_keyword(self, node):
+        pass
+
+    def visit_tuple(self, node):
+        for value in node.values:
+            self.visit(value)
+
+    def visit_list(self, node):
+        for value in node.values:
+            self.visit(value)
+
+    def visit_dict(self, node):
+        for value in node.values:
+            self.visit(value)
+
+
+class NodeTransformer(object):
+
+    def visit(self, node):
+        if hasattr(node, 'accept'):
+            return node.accept(self)
+        else:
+            return self.generic_visit(node)
+
+    def generic_visit(self, node):
+        return node
+
+    def visit_symbol(self, node):
+        return Symbol(node.name)
+
+    def visit_keyword(self, node):
+        return Keyword(node.name)
+
+    def visit_tuple(self, node):
+        return Tuple(self.visit(value)
+                     for value in node.values)
+
+    def visit_list(self, node):
+        return List(self.visit(value)
+                    for value in node.values)
+
+    def visit_dict(self, node):
+        return Dict(self.visit(value)
+                    for value in node.values)
