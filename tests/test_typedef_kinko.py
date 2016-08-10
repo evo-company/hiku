f2f886c6324dc191a6fa647c62b68e027a38ac97
Vladimir Magamedov <vladimir@magamedov.com>
2016-08-10 18:50:24 +0300

    Refactored Kinko typedef tests and added test for complex fields

--- tests/test_typedef_kinko.py
+++ tests/test_typedef_kinko.py
@@ -2,175 +2,203 @@ import difflib
 from textwrap import dedent
 
 from hiku.graph import Graph, Edge, Field, Link, Root, Many, One
-from hiku.types import Sequence, Mapping, Integer, String, Optional
+from hiku.types import Sequence, Mapping, Integer, String, Optional, Record
 from hiku.typedef.kinko import dumps
 
-from .base import TestCase
 
-
-def noop(*args, **kwargs):
+def _(*args, **kwargs):
     raise NotImplementedError
 
 
-class TestTypeDefKinko(TestCase):
+def assert_dumps(root, schema):
+    first = dumps(root)
+    second = dedent(schema).strip() + '\n'
+    if first != second:
+        msg = ('Dumped schema mismatches:\n\n{}'
+               .format('\n'.join(difflib.ndiff(first.splitlines(),
+                                               second.splitlines()))))
+        raise AssertionError(msg)
 
-    def assertDumps(self, root, schema):
-        first = dumps(root)
-        second = dedent(schema).strip() + '\n'
-        if first != second:
-            msg = ('Dumped schema mismatches:\n\n{}'
-                   .format('\n'.join(difflib.ndiff(first.splitlines(),
-                                                   second.splitlines()))))
-            raise self.failureException(msg)
 
-    def testField(self):
-        self.assertDumps(
-            Graph([Root([
-                Field('A', String, noop),
-            ])]),
-            """
-            type A String
-            """,
-        )
+def test_field():
+    assert_dumps(
+        Graph([Root([
+            Field('leones', String, _),
+        ])]),
+        """
+        type leones String
+        """,
+    )
 
-    def testEdge(self):
-        self.assertDumps(
-            Graph([
-                Edge('Foo', [
-                    Field('a', String, noop),
-                    Field('c', String, noop),
-                ]),
-                Edge('Bar', [
-                    Field('d', String, noop),
-                    Field('b', String, noop),
-                ]),
+
+def test_field_complex():
+    assert_dumps(
+        Graph([Root([
+            Field('behave', Optional[Record[{'burieth': Integer}]], _),
+            Field('gemara', Record[{'trevino': Integer}], _),
+            Field('riffage', Sequence[Record[{'shophar': Integer}]], _),
+        ])]),
+        """
+        type behave
+          Option
+            Record
+              :burieth Integer
+
+        type gemara
+          Record
+            :trevino Integer
+
+        type riffage
+          List
+            Record
+              :shophar Integer
+        """,
+    )
+
+
+def test_edge():
+    assert_dumps(
+        Graph([
+            Edge('adder', [
+                Field('kott', String, _),
+                Field('aseptic', String, _),
             ]),
-            """
-            type Foo
-              Record
-                :a String
-                :c String
-
-            type Bar
-              Record
-                :d String
-                :b String
-            """,
-        )
-
-    def testListSimple(self):
-        self.assertDumps(
-            Graph([Root([
-                Field('A', Sequence[Integer], noop),
-            ])]),
-            """
-            type A
-              List Integer
-            """,
-        )
-
-    def testListComplex(self):
-        self.assertDumps(
-            Graph([Root([
-                Field('A', Sequence[Sequence[Integer]], noop),
-            ])]),
-            """
-            type A
-              List
-                List Integer
-            """,
-        )
-
-    def testDictSimple(self):
-        self.assertDumps(
-            Graph([Root([
-                Field('A', Mapping[String, Integer], noop),
-            ])]),
-            """
-            type A
-              Dict String Integer
-            """,
-        )
-
-    def testDictComplex(self):
-        self.assertDumps(
-            Graph([Root([
-                Field('A', Mapping[String, Mapping[Integer, Integer]],
-                      noop),
-            ])]),
-            """
-            type A
-              Dict String
-                Dict Integer Integer
-            """,
-        )
-
-    def testTypeRef(self):
-        self.assertDumps(
-            Graph([
-                Edge('Foo', [
-                    Field('a', String, noop),
-                ]),
-                Edge('Bar', [
-                    Field('b', String, noop),
-                    Link('c', One, noop, edge='Foo', requires=None),
-                ]),
-                Edge('Baz', [
-                    Field('d', String, noop),
-                    Link('e', Many, noop, edge='Foo', requires=None),
-                ]),
+            Edge('brayden', [
+                Field('unhot', String, _),
+                Field('linea', String, _),
             ]),
-            """
-            type Foo
-              Record
-                :a String
+        ]),
+        """
+        type adder
+          Record
+            :kott String
+            :aseptic String
 
-            type Bar
-              Record
-                :b String
-                :c Foo
+        type brayden
+          Record
+            :unhot String
+            :linea String
+        """,
+    )
 
-            type Baz
-              Record
-                :d String
-                :e
-                  List Foo
-            """,
-        )
 
-    def testDocs(self):
-        self.assertDumps(
-            Graph([
-                Edge('Foo', [
-                    Field('a', String, noop, description="Attribute a"),
-                ], description="Some Foo explanation"),
-                Edge('Bar', [
-                    Field('b', Optional[String], noop,
-                          description="Attribute b"),
-                    Link('c', One, noop, edge='Foo', requires=None,
-                         description="Link c to Foo"),
-                ], description="Some Bar explanation"),
-                Edge('Baz', [
-                    Field('d', String, noop, description="Attribute d"),
-                    Link('e', Many, noop, edge='Foo', requires=None,
-                         description="Link e to Foo"),
-                ], description="Some Baz explanation"),
+def test_list_simple():
+    assert_dumps(
+        Graph([Root([
+            Field('askest', Sequence[Integer], _),
+        ])]),
+        """
+        type askest
+          List Integer
+        """,
+    )
+
+
+def test_list_complex():
+    assert_dumps(
+        Graph([Root([
+            Field('gladden', Sequence[Sequence[Integer]], _),
+        ])]),
+        """
+        type gladden
+          List
+            List Integer
+        """,
+    )
+
+
+def test_dict_simple():
+    assert_dumps(
+        Graph([Root([
+            Field('kasseri', Mapping[String, Integer], _),
+        ])]),
+        """
+        type kasseri
+          Dict String Integer
+        """,
+    )
+
+
+def test_dict_complex():
+    assert_dumps(
+        Graph([Root([
+            Field('trunks', Mapping[String, Mapping[Integer, Integer]], _),
+        ])]),
+        """
+        type trunks
+          Dict String
+            Dict Integer Integer
+        """,
+    )
+
+
+def test_type_ref():
+    assert_dumps(
+        Graph([
+            Edge('xeric', [
+                Field('derrida', String, _),
             ]),
-            """
-            type Foo  ; Some Foo explanation
-              Record
-                :a String  ; Attribute a
+            Edge('amb', [
+                Field('loor', String, _),
+                Link('cressy', One, _, edge='xeric', requires=None),
+            ]),
+            Edge('offeree', [
+                Field('abila', String, _),
+                Link('ferber', Many, _, edge='xeric', requires=None),
+            ]),
+        ]),
+        """
+        type xeric
+          Record
+            :derrida String
 
-            type Bar  ; Some Bar explanation
-              Record
-                :b  ; Attribute b
-                  Option String
-                :c Foo  ; Link c to Foo
+        type amb
+          Record
+            :loor String
+            :cressy xeric
 
-            type Baz  ; Some Baz explanation
-              Record
-                :d String  ; Attribute d
-                :e  ; Link e to Foo
-                  List Foo
-            """,
-        )
+        type offeree
+          Record
+            :abila String
+            :ferber
+              List xeric
+        """,
+    )
+
+
+def testDocs():
+    assert_dumps(
+        Graph([
+            Edge('switzer', [
+                Field('beatch', String, _, description="attribute beatch"),
+            ], description="switzer description"),
+            Edge('trine', [
+                Field('propels', Optional[String], _,
+                      description="attribute propels"),
+                Link('cardura', One, _, edge='switzer', requires=None,
+                     description="link cardura to switzer"),
+            ], description="trine description"),
+            Edge('packrat', [
+                Field('pikes', String, _, description="attribute pikes"),
+                Link('albus', Many, _, edge='switzer', requires=None,
+                     description="link albus to switzer"),
+            ], description="packrat description"),
+        ]),
+        """
+        type switzer  ; switzer description
+          Record
+            :beatch String  ; attribute beatch
+
+        type trine  ; trine description
+          Record
+            :propels  ; attribute propels
+              Option String
+            :cardura switzer  ; link cardura to switzer
+
+        type packrat  ; packrat description
+          Record
+            :pikes String  ; attribute pikes
+            :albus  ; link albus to switzer
+              List switzer
+        """,
+    )
