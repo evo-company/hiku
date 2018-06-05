ebc5d9857c909e3b22957f7d68e0dfac79f5e1c9
Vladimir Magamedov <vladimir@magamedov.com>
2018-06-05 15:23:42 +0300

    Slightly improved package configs, grpcio now don't requires protobuf package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Hiku',
+    name='hiku',
     version='0.4.2',
     description='Library to implement Graph APIs',
     author='Vladimir Magamedov',
     author_email='vladimir@magamedov.com',
     url='https://github.com/vmagamedov/hiku',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test*']),
     include_package_data=True,
     license='BSD',
     install_requires=[],
