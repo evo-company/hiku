9032bee94bcb110f7ea646a038d9b685331e998e
Vladimir Magamedov <vladimir@magamedov.com>
2018-10-05 15:43:48 +0300

    Issued release candidate 0.5.0rc3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hiku',
-    version='0.5.0rc2',
+    version='0.5.0rc3',
     description='Library to implement Graph APIs',
     author='Vladimir Magamedov',
     author_email='vladimir@magamedov.com',
