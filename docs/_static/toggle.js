6a91ccacefec03b9c7b7e4f87865dd9a8d331c38
Vladimir Magamedov <vladimir@magamedov.com>
2018-10-30 18:56:41 +0200

    Removed unnecessary sphinx extensions

--- docs/_static/toggle.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".toggle > *").hide();
-    $(".toggle .header").show().click(function() {
-        $(this).parent().children().not(".header").toggle(100);
-        $(this).parent().children(".header").toggleClass("open");
-    })
-});
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx_tabs.tabs',
 ]
 
 autoclass_content = 'both'
@@ -32,4 +31,3 @@ html_theme_options = {
 def setup(app):
     app.add_stylesheet('style.css?r=1')
     app.add_stylesheet('fixes.css?r=1')
-    app.add_javascript('toggle.js?r=1')
