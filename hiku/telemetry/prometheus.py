e91f968aae46eea2df80dd3d0eb43ee8382270b2
Vladimir Magamedov <vladimir@magamedov.com>
2019-10-01 17:10:04 +0300

    Added Prometheus metrics support, updated requirements

---
+++ hiku/telemetry/prometheus.py
@@ -0,0 +1,187 @@
+import time
+from abc import abstractmethod
+from functools import partial
+
+from prometheus_client import Summary
+
+from ..graph import GraphTransformer
+from ..engine import pass_context, _do_pass_context
+from ..sources.graph import CheckedExpr
+
+
+def _func_field_names(func):
+    fields_pos = 1 if _do_pass_context(func) else 0
+
+    def wrapper(*args):
+        return func([f.name for f in args[fields_pos]], *args)
+    return wrapper
+
+
+def _subquery_field_names(func):
+    def wrapper(fields, *args):
+        return func([f.name for _, f in fields], fields, *args)
+    return wrapper
+
+
+class GraphMetricsBase(GraphTransformer):
+    root_name = 'Root'
+
+    _metric = Summary(
+        'graph_field_time',
+        'Graph field time (seconds)',
+        ['graph', 'node', 'field'],
+    )
+
+    def __init__(self, name):
+        self._name = name
+        self._node = None
+        self._wrappers = {}
+
+    @abstractmethod
+    def field_wrapper(self, observe, func):
+        raise NotImplementedError
+
+    @abstractmethod
+    def link_wrapper(self, observe, func):
+        raise NotImplementedError
+
+    @abstractmethod
+    def subquery_wrapper(self, observe, subquery):
+        raise NotImplementedError
+
+    def _observe_fields(self, node_name):
+        by_field = {}
+
+        def observe(start_time, field_names):
+            duration = time.perf_counter() - start_time
+            for name in field_names:
+                try:
+                    field_metric = by_field[name]
+                except KeyError:
+                    field_metric = by_field[name] = \
+                        self._metric.labels(self._name, node_name, name)
+                field_metric.observe(duration)
+        return observe
+
+    def _wrap_field(self, node_name, func):
+        observe = self._observe_fields(node_name)
+        wrapper = self.field_wrapper(observe, func)
+        if _do_pass_context(func):
+            wrapper = pass_context(wrapper)
+        wrapper = _func_field_names(wrapper)
+        if _do_pass_context(func):
+            wrapper = pass_context(wrapper)
+        return wrapper
+
+    def _wrap_link(self, node_name, link_name, func):
+        observe = self._observe_fields(node_name)
+        wrapper = self.link_wrapper(observe, func)
+        if _do_pass_context(func):
+            wrapper = pass_context(wrapper)
+        wrapper = partial(wrapper, link_name)
+        if _do_pass_context(func):
+            wrapper = pass_context(wrapper)
+        return wrapper
+
+    def _wrap_subquery(self, node_name, subquery):
+        observe = self._observe_fields(node_name)
+        wrapper = self.subquery_wrapper(observe, subquery)
+        wrapper = _subquery_field_names(wrapper)
+        wrapper.__subquery__ = lambda: wrapper
+        return wrapper
+
+    def visit_node(self, obj):
+        self._node = obj
+        try:
+            return super().visit_node(obj)
+        finally:
+            self._node = None
+
+    def visit_field(self, obj):
+        obj = super().visit_field(obj)
+        node_name = self.root_name if self._node is None else self._node.name
+        if isinstance(obj.func, CheckedExpr):
+            func = obj.func.__subquery__
+        else:
+            func = obj.func
+
+        wrapper = self._wrappers.get(func)
+        if wrapper is None:
+            if isinstance(obj.func, CheckedExpr):
+                wrapper = self._wrappers[func] = self._wrap_subquery(
+                    node_name, func,
+                )
+            else:
+                wrapper = self._wrappers[func] = self._wrap_field(
+                    node_name, func,
+                )
+
+        if isinstance(obj.func, CheckedExpr):
+            obj.func = CheckedExpr(
+                wrapper,
+                obj.func.expr,
+                obj.func.reqs,
+                obj.func.proc,
+            )
+        else:
+            obj.func = wrapper
+        return obj
+
+    def visit_link(self, obj):
+        obj = super().visit_link(obj)
+        node_name = self.root_name if self._node is None else self._node.name
+        obj.func = self._wrap_link(node_name, obj.name, obj.func)
+        return obj
+
+
+class _SubqueryMixin:
+
+    def subquery_wrapper(self, observe, subquery):
+        def wrapper(field_names, *args):
+            start_time = time.perf_counter()
+            result_proc = subquery(*args)
+
+            def proc_wrapper():
+                result = result_proc()
+                observe(start_time, field_names)
+                return result
+            return proc_wrapper
+        return wrapper
+
+
+class GraphMetrics(_SubqueryMixin, GraphMetricsBase):
+
+    def field_wrapper(self, observe, func):
+        def wrapper(field_names, *args):
+            start_time = time.perf_counter()
+            result = func(*args)
+            observe(start_time, field_names)
+            return result
+        return wrapper
+
+    def link_wrapper(self, observe, func):
+        def wrapper(link_name, *args):
+            start_time = time.perf_counter()
+            result = func(*args)
+            observe(start_time, [link_name])
+            return result
+        return wrapper
+
+
+class AsyncGraphMetrics(_SubqueryMixin, GraphMetricsBase):
+
+    def field_wrapper(self, observe, func):
+        async def wrapper(field_names, *args):
+            start_time = time.perf_counter()
+            result = await func(*args)
+            observe(start_time, field_names)
+            return result
+        return wrapper
+
+    def link_wrapper(self, observe, func):
+        async def wrapper(link_name, *args):
+            start_time = time.perf_counter()
+            result = await func(*args)
+            observe(start_time, [link_name])
+            return result
+        return wrapper
---
+++ tests/test_telemetry_prometheus.py
@@ -0,0 +1,178 @@
+import faker
+import pytest
+
+from prometheus_client import REGISTRY
+
+from hiku import query as q
+from hiku.graph import Graph, Node, Field, Link, Root, apply
+from hiku.types import TypeRef
+from hiku.engine import Engine, pass_context
+from hiku.sources.graph import SubGraph
+from hiku.executors.sync import SyncExecutor
+from hiku.executors.asyncio import AsyncIOExecutor
+from hiku.telemetry.prometheus import GraphMetrics, AsyncGraphMetrics
+
+from tests.base import check_result
+
+
+fake = faker.Faker()
+
+
+@pytest.fixture(name='graph_name')
+def graph_name_fixture():
+    return fake.pystr()
+
+
+@pytest.fixture(name='sample_count')
+def sample_count_fixture(graph_name):
+    def sample_count(node, field):
+        return REGISTRY.get_sample_value(
+            'graph_field_time_count',
+            dict(graph=graph_name, node=node, field=field),
+        )
+    return sample_count
+
+
+def test_simple_sync(graph_name, sample_count):
+
+    def x_fields(fields, ids):
+        return [[42 for _ in fields] for _ in ids]
+
+    def root_fields(fields):
+        return [1 for _ in fields]
+
+    def x_link():
+        return 2
+
+    ll_graph = Graph([
+        Node('X', [
+            Field('id', None, x_fields),
+        ]),
+    ])
+
+    x_sg = SubGraph(ll_graph, 'X')
+
+    hl_graph = Graph([
+        Node('X', [
+            Field('id', None, x_sg),
+        ]),
+        Root([
+            Field('a', None, root_fields),
+            Link('x', TypeRef['X'], x_link, requires=None),
+        ]),
+    ])
+
+    hl_graph = apply(hl_graph, [GraphMetrics(graph_name)])
+
+    assert sample_count('Root', 'a') is None
+    assert sample_count('Root', 'x') is None
+    assert sample_count('X', 'id') is None
+
+    result = Engine(SyncExecutor()).execute(hl_graph, q.Node([
+        q.Field('a'),
+        q.Link('x', q.Node([
+            q.Field('id'),
+        ])),
+    ]))
+    check_result(result, {
+        'a': 1,
+        'x': {
+            'id': 42,
+        },
+    })
+
+    assert sample_count('Root', 'a') == 1.0
+    assert sample_count('Root', 'x') == 1.0
+    assert sample_count('X', 'id') == 1.0
+
+
+@pytest.mark.asyncio
+async def test_simple_async(graph_name, sample_count):
+
+    async def x_fields(fields, ids):
+        return [[42 for _ in fields] for _ in ids]
+
+    async def root_fields(fields):
+        return [1 for _ in fields]
+
+    async def x_link():
+        return 2
+
+    ll_graph = Graph([
+        Node('X', [
+            Field('id', None, x_fields),
+        ]),
+    ])
+
+    x_sg = SubGraph(ll_graph, 'X')
+
+    hl_graph = Graph([
+        Node('X', [
+            Field('id', None, x_sg),
+        ]),
+        Root([
+            Field('a', None, root_fields),
+            Link('x', TypeRef['X'], x_link, requires=None),
+        ]),
+    ])
+
+    hl_graph = apply(hl_graph, [AsyncGraphMetrics(graph_name)])
+
+    query = q.Node([
+        q.Field('a'),
+        q.Link('x', q.Node([
+            q.Field('id'),
+        ])),
+    ])
+
+    engine = Engine(AsyncIOExecutor())
+
+    assert sample_count('Root', 'a') is None
+    assert sample_count('Root', 'x') is None
+    assert sample_count('X', 'id') is None
+
+    result = await engine.execute(hl_graph, query)
+    check_result(result, {
+        'a': 1,
+        'x': {
+            'id': 42,
+        },
+    })
+
+    assert sample_count('Root', 'a') == 1.0
+    assert sample_count('Root', 'x') == 1.0
+    assert sample_count('X', 'id') == 1.0
+
+
+def test_with_pass_context(graph_name, sample_count):
+
+    def root_fields1(fields):
+        return [1 for _ in fields]
+
+    @pass_context
+    def root_fields2(ctx, fields):
+        return [2 for _ in fields]
+
+    graph = Graph([
+        Root([
+            Field('a', None, root_fields1),
+            Field('b', None, root_fields2),
+        ]),
+    ])
+
+    graph = apply(graph, [GraphMetrics(graph_name)])
+
+    assert sample_count('Root', 'a') is None
+    assert sample_count('Root', 'b') is None
+
+    result = Engine(SyncExecutor()).execute(graph, q.Node([
+        q.Field('a'),
+        q.Field('b'),
+    ]))
+    check_result(result, {
+        'a': 1,
+        'b': 2,
+    })
+
+    assert sample_count('Root', 'a') == 1.0
+    assert sample_count('Root', 'b') == 1.0
