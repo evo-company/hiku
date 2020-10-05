3b34f1908208a06bb6424758195d031c90452d3a
Vladimir Magamedov <vladimir@magamedov.com>
2020-10-05 15:45:35 +0300

    Removed old compatibility code

--- hiku/compat.py
+++ hiku/compat.py
@@ -2,26 +2,13 @@ import sys
 import ast as _ast
 
 
-PY36 = sys.version_info >= (3, 6)
 PY38 = sys.version_info >= (3, 8)
-try:
-    import __pypy__  # noqa
-    PYPY = True
-except ImportError:
-    PYPY = False
 
 
 class _AST:
 
     def __getattr__(self, name):
         return getattr(_ast, name)
-
-    if PY36:
-        comprehension = _ast.comprehension
-    else:
-        @staticmethod
-        def comprehension(target, iter, ifs, _):
-            return _ast.comprehension(target, iter, ifs)
 
     if PY38:
         arguments = _ast.arguments
