873eccb6674722272ce86b9bc1307c115a33a994
Max Kindritskiy <kindritskiy.m@gmail.com>
2021-06-21 18:17:22 +0300

    add _entities Field during FederatedGraph creation

--- federation/graph.py
+++ federation/graph.py
@@ -1,40 +1,36 @@
+from typing import (
+    List,
+    Optional,
+    Dict,
+)
+
 from hiku.graph import (
     Graph,
-    Root,
     Node,
-    Link,
+    Field,
+    Option,
+)
+from hiku.types import (
+    Record,
+    Sequence,
+    Union,
+    Any,
 )
 
 
-class ExtendLink(Link):
-    pass
-
-
-class ExtendNode(Node):
-    def __init__(self, name, fields, *, description=None, keys=None):
-        """
-        :param keys: primary keys for entity
-            https://www.apollographql.com/docs/federation/federation-spec/#key
-        """
-        super().__init__(name, fields, description=description)
-        self.keys = keys
-
-
 class FederatedGraph(Graph):
-    def __init__(self, items, data_types=None):
-        self.extend_nodes = self.get_extend_nodes(items)
-        self.extend_links = self.get_extend_links(self.get_root(items).fields)
-        self.extend_node_keys_map = {node.name: node.keys for node
-                                     in self.extend_nodes}
+    def __init__(
+            self,
+            items: List[Node],
+            data_types: Optional[Dict[str, 'Record']] = None,
+    ):
+        for item in items:
+            if item.name is None:
+                item.fields.append(
+                    Field('_entities', Sequence[Union['_Entity']], lambda: None,
+                          options=[
+                              Option('representations', Sequence[Any['_Any']])
+                          ])
+                )
 
         super().__init__(items, data_types)
-
-    def get_extend_nodes(self, items):
-        return [item for item in items if isinstance(item, ExtendNode)]
-
-    def get_extend_links(self, root_fields):
-        return [field for field in root_fields if isinstance(field, ExtendLink)]
-
-    def get_root(self, items):
-        # TODO no validation, be careful
-        return [item for item in items if isinstance(item, Root)][0]
