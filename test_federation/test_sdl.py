8674efaa44b01c0c31559ef704ee70140c43bafc
Max Kindritskiy <kindritskiy.m@gmail.com>
2021-06-22 10:16:30 +0300

    refactor test_sdl

--- test_federation/test_sdl.py
+++ test_federation/test_sdl.py
@@ -1,25 +1,28 @@
 from unittest import TestCase
 
+from federation.directive import KeyDirective
 from federation.endpoint import FederatedGraphQLEndpoint
 from federation.engine import Engine
 from federation.graph import (
-    ExtendLink,
     FederatedGraph,
-    ExtendNode,
 )
+from federation.introspection import FederatedGraphQLIntrospection
 from federation.sdl import print_sdl
-from federation.service import print_service_sdl
 from hiku.executors.sync import SyncExecutor
 from hiku.graph import (
     Option,
     Root,
     Field,
+    apply,
+    Node,
+    Link,
 )
 from hiku.types import (
     Integer,
     String,
     TypeRef,
     Sequence,
+    Optional,
 )
 
 
@@ -27,17 +30,17 @@ def mock_link(): pass
 def mock_resolver(): pass
 
 
-AstronautLink = ExtendLink(
+AstronautLink = Link(
     'astronaut',
     TypeRef['Astronaut'],
     mock_link,
     requires=None,
     options=[
-        Option('id', Integer)
+        Option('id', Optional[Integer])
     ],
 )
 
-AstronautsLink = ExtendLink(
+AstronautsLink = Link(
     'astronauts',
     Sequence[TypeRef['Astronaut']],
     mock_link,
@@ -45,20 +48,26 @@ AstronautsLink = ExtendLink(
     options=None,
 )
 
+AstronautNode = Node('Astronaut', [
+    Field('id', Integer, mock_resolver),
+    Field('name', String, mock_resolver),
+    Field('age', Integer, mock_resolver),
+], directives=[
+    KeyDirective('id')
+])
+
 ROOT_FIELDS = [
     AstronautLink,
     AstronautsLink,
 ]
 
-AstronautNode = ExtendNode('Astronaut', [
-    Field('id', Integer, mock_resolver),
-    Field('name', String, mock_resolver),
-    Field('age', Integer, mock_resolver),
-], keys=['id'])
-
 GRAPH = FederatedGraph([
     AstronautNode,
     Root(ROOT_FIELDS),
+])
+
+INTROSPECTED_GRAPH = apply(GRAPH, [
+    FederatedGraphQLIntrospection(GRAPH),
 ])
 
 
@@ -72,45 +81,32 @@ def execute(graph, query_string):
 
 
 class TestSDL(TestCase):
-    def test_resolve_sdl_correct(self):
-        sdl = print_service_sdl(ROOT_FIELDS, [AstronautNode])
-        expected = (
-            'type Astronaut @key(fields: "id") {\n  '
-            'id: Int!\n  '
-            'name: String!\n  '
-            'age: Int!\n}\n\n'
-            'extend type Query { '
-            'astronaut(id: Int): Astronaut!\n\n'
-            'astronauts: [Astronaut!]!\n \n}'
-        )
-        self.assertEqual(sdl, expected)
+    def test_print_graph_sdl(self):
+        sdlected = print_sdl(GRAPH)
+        expected = [
+            'type Astronaut @key(fields: "id") {',
+            '  id: Int!',
+            '  name: String!',
+            '  age: Int!', '}',
+            '',
+            'extend type Query {',
+            '  astronaut(id: Int): Astronaut!',
+            '  astronauts: [Astronaut!]!',
+            '}'
+        ]
+        self.assertEqual(sdlected.splitlines(), expected)
 
-    def test_print_sdl_for_extended_link(self):
-        sdl = print_sdl(AstronautLink)
-        expected = 'astronaut(id: Int): Astronaut!\n'
-        self.assertEqual(sdl, expected)
-
-        sdl = print_sdl(AstronautsLink)
-        expected = 'astronauts: [Astronaut!]!\n'
-        self.assertEqual(sdl, expected)
-
-    def test_print_sdl_for_extended_node(self):
-        sdl = print_sdl(AstronautNode)
-        exp = (
-            'type Astronaut @key(fields: "id") '
-            '{\n  id: Int!\n  name: String!\n  age: Int!\n}\n'
-        )
-        self.assertEqual(sdl, exp)
-
-    def test_query_service_sdl(self):
-        result = execute(GRAPH, {'query': '{ _service { sdl } }'})
-        expected = (
-            'type Astronaut @key(fields: \"id\") {\n  '
-            'id: Int!\n  '
-            'name: String!\n  '
-            'age: Int!\n}\n\n'
-            'extend type Query { '
-            'astronaut(id: Int): Astronaut!\n\n'
-            'astronauts: [Astronaut!]!\n \n}'
-        )
-        self.assertEqual(result['data'], {'_service': {'sdl': expected}})
+    def test_print_introspected_graph_sdl(self):
+        sdlected = print_sdl(INTROSPECTED_GRAPH)
+        expected = [
+            'type Astronaut @key(fields: "id") {',
+            '  id: Int!',
+            '  name: String!',
+            '  age: Int!', '}',
+            '',
+            'extend type Query {',
+            '  astronaut(id: Int): Astronaut!',
+            '  astronauts: [Astronaut!]!',
+            '}'
+        ]
+        self.assertEqual(sdlected.splitlines(), expected)
